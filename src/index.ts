7a52b21dfb390e7df972182ff9d524f6d12bd4b4
Michal Ozogán <michal@ozogan.eu>
2021-07-18 10:54:00 +0000

    Changed: Remove Endomondo

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { ContainerModule, interfaces } from 'inversify';
 import 'reflect-metadata';
-import { EndomondoConvertor, EndomondoTransferAdapter } from './services/transfer-adapters/endomondo';
 import { FitbitConvertor, FitbitTransferAdapter } from './services/transfer-adapters/fitbit';
 import { GarminConvertor, GarminTransferAdapter } from './services/transfer-adapters/garmin';
 import { StravaConvertor, StravaTransferAdapter } from './services/transfer-adapters/strava';
 
 export const ADAPTERS = {
     GarminTransferAdapter,
-    EndomondoTransferAdapter,
     FitbitTransferAdapter,
     StravaTransferAdapter,
 };
@@ -16,12 +14,10 @@ export { default as WorkoutTransfer } from './services/WorkoutTransfer';
 
 export default new ContainerModule((bind: interfaces.Bind) => {
     bind(GarminConvertor).toSelf();
-    bind(EndomondoConvertor).toSelf();
     bind(FitbitConvertor).toSelf();
     bind(StravaConvertor).toSelf();
 
     bind(GarminTransferAdapter).toSelf();
-    bind(EndomondoTransferAdapter).toSelf();
     bind(FitbitTransferAdapter).toSelf();
     bind(StravaTransferAdapter).toSelf();
 });
--- src/services/transfer-adapters/endomondo/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as EndomondoConvertor } from './EndomondoConvertor';
-export { default as EndomondoTransferAdapter } from './EndomondoTransferAdapter';
